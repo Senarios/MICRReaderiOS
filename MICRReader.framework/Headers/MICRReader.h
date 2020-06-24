7f6e1c25ab952b69decc4b97ade3f81f413f057f
Senarios <63293682+Senarios@users.noreply.github.com>
2020-06-24 17:00:16 +0500

    Add files via upload

---
+++ MICRReader.framework/Headers/MICRReader.h
@@ -0,0 +1,19 @@
+//
+//  MICRReader.h
+//  MICRReader
+//
+//  Created by APPLE on 18/06/2020.
+//  Copyright © 2020 Senarios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MICRReader.
+FOUNDATION_EXPORT double MICRReaderVersionNumber;
+
+//! Project version string for MICRReader.
+FOUNDATION_EXPORT const unsigned char MICRReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MICRReader/PublicHeader.h>
+
+
